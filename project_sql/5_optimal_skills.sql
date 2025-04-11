71f0521c48bd3b8639259c81ee5466e49e03aae2
Andrea Cazares <andreacazares1412@gmail.com>
2025-04-11 12:08:48 -0600

    Update 5_optimal_skills.sql

--- project_sql/5_optimal_skills.sql
+++ project_sql/5_optimal_skills.sql
@@ -6,21 +6,23 @@ Answer: What are the most optimal skills to learn (aka it’s in high demand and
     offering strategic insights for career development in data analysis
 */
 
+WITH highest_payed_skill AS (
 SELECT 
     skills_dim.skill_id,
-    skills_dim.skills, 
-    COUNT(*) AS demand_count, 
-    ROUND(AVG(salary_year_avg), 0) AS salary_avg
-FROM job_postings_fact
-INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
-INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    skills_dim.skills,
+    COUNT(skills_dim.skills) AS skill_demand,
+    ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS salary_avg
+FROM skills_job_dim
+LEFT JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+LEFT JOIN job_postings_fact ON skills_job_dim.job_id = job_postings_fact.job_id
 WHERE 
-    job_title_short = 'Data Analyst' 
-    AND salary_year_avg IS NOT NULL
-    AND job_work_from_home = True
-GROUP BY skills_dim.skill_id
-HAVING COUNT(*) > 10
-ORDER BY 
-    salary_avg DESC,
-    demand_count DESC
-LIMIT 25;
+    job_postings_fact.salary_year_avg IS NOT NULL 
+    AND job_title_short = 'Data Analyst'
+GROUP BY skills_dim.skills,  skills_dim.skill_id)
+
+
+SELECT highest_payed_skill.*,
+    ROUND(highest_payed_skill.salary_avg * LOG(highest_payed_skill.skill_demand)) AS score
+FROM highest_payed_skill 
+ORDER BY score DESC
+LIMIT 10;
